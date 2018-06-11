7abac0c956784e76431119e9dde0c7614cfde609
WangHang <from-wh@hotmail.com>
2018-06-11 18:01:58 +0800

    添加登录、注册及相关css

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,9 @@
-var app = new Vue({
+let app = new Vue({
   el: '#page',
   data: {
     editting: false,
+    loginVisible:false,
+    signUpVisible:false,
     resume: {
       name: '王航',
       jobTitle: '前端开发工程师',
@@ -10,11 +12,42 @@ var app = new Vue({
       emal: 'from-wh@hotmail.com',
       phone: '1234556789',
     },
-    
+
   },
   methods: {
     onEdit(key, value) {
       this.resume[key] = value  //console.log(this === app)
-    }
+    },
+    onClickSave() {
+      var currentUser = AV.User.current();
+      if (!currentUser) {
+         this.showLogin()
+      }
+      else {
+        saveResume()
+      }
+      // // 声明类型
+      // let User = AV.Object.extend('User')
+      // // 新建对象
+      // let user = new User()
+      // // 设置名称
+      // user.set('name', '工作')
+      // // 设置优先级
+      // user.set('priority', 1)
+      // user.save().then(function (todo) {
+      //   console.log('objectId is ' + todo.id);
+      // }, function (error) {
+      //   console.error(error)
+      // })
+    },
+    showLogin(){
+      this.loginVisible = true
+    },
+    showsignUp(){
+      this.signUpVisible = true
+    },
+    saveResume(){
+
+    },
   }
 })
